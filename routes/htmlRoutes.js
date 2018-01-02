11a7d55bcaf8d4b4650586bed244375d287e5491
Shane Orend <wagen00@gmail.com>
2018-01-02 16:03:28 -0500

    added server logic and routes folder with coressponding routes in the html files pages using canavas now appear blank ??

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,33 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+	app.get("/login", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/login.html"));
+	});
+
+	app.get("/contact", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/contact.html"));
+	});
+
+	app.get("/scores", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/scorePage.html"));
+	});
+
+	app.get("/game", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/game.html"));
+	});
+
+	app.get("/arena", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/arena.html"));
+	});
+
+	app.get("/extra", function(req, res){
+		res.sendFile(path.join(__dirname, "../otherPages/bonus.html"));
+	});
+
+	app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+
+};
